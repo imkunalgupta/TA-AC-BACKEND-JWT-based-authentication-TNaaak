1bba1716cd2c68b027768a694b06f4cd25620f44
Kunal <mailto.guptakunal@gmail.com>
2022-12-16 00:45:36 +0530

    finish

--- block-BNaafs/jwt-basics/routes/index.js
+++ block-BNaafs/jwt-basics/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var auth = require('../middlewares/auth');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  //res.render('index', { title: 'Express' });
+  res.status(200).json({ msg: 'Welcome' });
+});
+
+router.get('/dashboard', auth.verifyToken, (req, res) => {
+  //console.log(req.user);
+  res.json({ access: 'Protected Resource' });
 });
 
 module.exports = router;
